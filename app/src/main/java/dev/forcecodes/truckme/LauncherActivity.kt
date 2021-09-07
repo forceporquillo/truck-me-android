a979ab47cc6049b68886ab4558ecd148f3e5b241
strongforce1 <fporquillo18@gmail.com>
2021-09-07 18:21:53 +0800

    Initial commit

---
+++ app/src/main/java/dev/forcecodes/truckme/LauncherActivity.kt
@@ -0,0 +1,18 @@
+package dev.forcecodes.truckme
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import dev.forcecodes.truckme.databinding.LauncherBinding
+import dev.forcecodes.truckme.ui.auth.AuthActivity
+import dev.forcecodes.truckme.ui.auth.SignInFragment
+
+class LauncherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
