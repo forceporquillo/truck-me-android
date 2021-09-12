32414c5590cd138e1f357365f2326eeca09204a5
strongforce1 <fporquillo18@gmail.com>
2021-09-12 18:16:50 +0800

    Add LauncherActivity

--- app/src/main/java/dev/forcecodes/truckme/LauncherActivity.kt
+++ app/src/main/java/dev/forcecodes/truckme/LauncherActivity.kt
@@ -2,20 +2,48 @@ package dev.forcecodes.truckme
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import dev.forcecodes.truckme.databinding.LauncherBinding
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import dagger.hilt.android.AndroidEntryPoint
 import dev.forcecodes.truckme.ui.auth.AuthActivity
-import dev.forcecodes.truckme.ui.auth.SignInFragment
+import dev.forcecodes.truckme.ui.auth.signin.SignInViewModelDelegate
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import kotlin.reflect.KClass
 
+@AndroidEntryPoint
 class LauncherActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var signInViewModelDelegate: SignInViewModelDelegate
+
+    @FlowPreview
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
 
-        Handler(Looper.getMainLooper()).postDelayed({
-         startActivity(Intent(this, AuthActivity::class.java))
-        }, 2500L)
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                signInViewModelDelegate.isUserSignedIn
+                    .debounce(1000L)
+                    .collectLatest { isSignedIn ->
+                        val actions = if (isSignedIn) {
+                            MainActivity::class
+                        } else {
+                            AuthActivity::class
+                        }
+                        navigationActions(actions)
+                    }
+            }
+        }
+    }
+
+    private fun navigationActions(kClass: KClass<out AppCompatActivity>) {
+        startActivity(Intent(this, kClass.java))
+        finish()
     }
 }
