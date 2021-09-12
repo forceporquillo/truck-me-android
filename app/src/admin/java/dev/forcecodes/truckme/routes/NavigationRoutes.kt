c16391ddf02a70606eb0730aac86a0c0b48791b2
strongforce1 <fporquillo18@gmail.com>
2021-09-13 07:51:43 +0800

    Update with different nav graphs id

--- app/src/admin/java/dev/forcecodes/truckme/routes/NavigationRoutes.kt
+++ app/src/admin/java/dev/forcecodes/truckme/routes/NavigationRoutes.kt
@@ -3,8 +3,8 @@ package dev.forcecodes.truckme.routes
 import dev.forcecodes.truckme.R
 
 val topDestinations = setOf(
-    R.id.home,
-    R.id.fleet,
+    R.id.homeFragment,
+    R.id.fleetFragment,
     R.id.statistics,
     R.id.history,
     R.id.account
