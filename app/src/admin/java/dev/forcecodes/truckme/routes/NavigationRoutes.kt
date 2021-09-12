58d94b9ca6238913be3f7cab4fef89c8a5929648
strongforce1 <fporquillo18@gmail.com>
2021-09-13 03:38:44 +0800

    Add top destinations for admin

---
+++ app/src/admin/java/dev/forcecodes/truckme/routes/NavigationRoutes.kt
@@ -0,0 +1,11 @@
+package dev.forcecodes.truckme.routes
+
+import dev.forcecodes.truckme.R
+
+val topDestinations = setOf(
+    R.id.home,
+    R.id.fleet,
+    R.id.statistics,
+    R.id.history,
+    R.id.account
+)
