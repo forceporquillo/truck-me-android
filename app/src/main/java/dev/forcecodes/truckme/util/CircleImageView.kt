7c29c3daee68da9afe80d113e061885111ac98eb
4s <fporquillo18@gmail.com>
2021-09-20 23:12:56 +0800

    Delete ExampleUnitTest.kt

--- app/src/main/java/dev/forcecodes/truckme/util/CircleImageView.kt
+++ app/src/main/java/dev/forcecodes/truckme/util/CircleImageView.kt
@@ -1,6 +1,6 @@
 @file:Suppress("unused")
 
-package dev.forcecodes.truckme.util.widgets
+package dev.forcecodes.truckme.util
 
 import android.annotation.SuppressLint
 import android.content.Context
