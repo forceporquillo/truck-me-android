fc1a673738aca3d77ae48b8a6637bcced1c90e40
4s <fporquillo18@gmail.com>
2021-09-29 03:05:09 +0800

    Add use cases for add deliveries and getting active jobs

---
+++ core/src/admin/java/dev/forcecodes/truckme/core/domain/dashboard/AddDeliveryUseCase.kt
@@ -0,0 +1,51 @@
+package dev.forcecodes.truckme.core.domain.dashboard
+
+import dev.forcecodes.truckme.core.data.delivery.DeliveryDataSource
+import dev.forcecodes.truckme.core.di.IoDispatcher
+import dev.forcecodes.truckme.core.domain.FlowUseCase
+import dev.forcecodes.truckme.core.model.DeliveryInfo
+import dev.forcecodes.truckme.core.util.Result
+import dev.forcecodes.truckme.core.util.TaskData
+import dev.forcecodes.truckme.core.util.triggerOneShotListener
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class AddDeliveryUseCase @Inject constructor(
+  private val deliveryDataSource: DeliveryDataSource,
+  @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : FlowUseCase<DeliveryInfo, AddDeliveryTask>(dispatcher) {
+
+  override fun execute(parameters: DeliveryInfo): Flow<Result<AddDeliveryTask>> {
+    return flow {
+      emit(Result.Loading)
+      val result = deliveryDataSource.addDelivery(parameters)
+        .triggerOneShotListener(AddDeliveryTask(null))!!
+      if (result.isSuccess) {
+        emit(Result.Success(result))
+      } else
+        emit(
+          Result.Error(
+            result.exception
+              ?: Exception("Unknown error.")
+          )
+        )
+    }
+  }
+}
+
+data class AddDeliveryTask(
+  override var data: Unit?,
+  override var isSuccess: Boolean = false,
+  override var exception: Exception? = null
+) : TaskData<Unit>
+
+data class DeliveryItems(
+  val id: String,
+  val timeStamp: String,
+  val driverName: String,
+  val destination: String,
+  val profileIcon: String? = "",
+  val eta: String
+)
---
+++ core/src/admin/java/dev/forcecodes/truckme/core/domain/dashboard/GetActiveJobsUseCase.kt
@@ -0,0 +1,29 @@
+package dev.forcecodes.truckme.core.domain.dashboard
+
+import dev.forcecodes.truckme.core.data.delivery.DeliveryDataSource
+import dev.forcecodes.truckme.core.di.IoDispatcher
+import dev.forcecodes.truckme.core.domain.FlowUseCase
+import dev.forcecodes.truckme.core.util.Result
+import dev.forcecodes.truckme.core.util.error
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetActiveJobsUseCase @Inject constructor(
+  private val deliveryDataSource: DeliveryDataSource,
+  private val activeJobDomainMapper: ActiveJobDomainMapper,
+  @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : FlowUseCase<Any, List<DeliveryItems>>(dispatcher) {
+
+  override fun execute(parameters: Any): Flow<Result<List<DeliveryItems>>> {
+    return deliveryDataSource.getActiveJobs().map { result ->
+      if (result is Result.Success) {
+        val deliverItems = result.data.map { activeJobDomainMapper.invoke(it) }
+        Result.Success(deliverItems)
+      } else {
+        Result.Error(result.error)
+      }
+    }
+  }
+}
