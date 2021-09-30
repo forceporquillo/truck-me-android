a2023fa4d133d765c645d77bf8f254de3b7d26ca
4s <fporquillo18@gmail.com>
2021-09-30 23:35:10 +0800

    Make history fragment and statistics fragment visible

--- app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsFragment.kt
+++ app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsFragment.kt
@@ -14,6 +14,6 @@ class StatisticsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
     }
 }
