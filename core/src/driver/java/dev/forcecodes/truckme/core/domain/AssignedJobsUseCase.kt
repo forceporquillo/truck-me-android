9997a6fc8b6ef8124ea40cc8faf002d43cd3801d
4s <fporquillo18@gmail.com>
2021-09-30 23:47:47 +0800

    Remove log

--- core/src/driver/java/dev/forcecodes/truckme/core/domain/AssignedJobsUseCase.kt
+++ core/src/driver/java/dev/forcecodes/truckme/core/domain/AssignedJobsUseCase.kt
@@ -3,17 +3,14 @@ package dev.forcecodes.truckme.core.domain
 import dev.forcecodes.truckme.core.data.ActiveJobItems
 import dev.forcecodes.truckme.core.data.AssignedDataSource
 import dev.forcecodes.truckme.core.data.driver.RegisteredDriverDataSource
-import dev.forcecodes.truckme.core.di.ApplicationScope
 import dev.forcecodes.truckme.core.di.IoDispatcher
 import dev.forcecodes.truckme.core.util.Result
 import dev.forcecodes.truckme.core.util.error
 import dev.forcecodes.truckme.core.util.successOr
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flatMapConcat
 import kotlinx.coroutines.flow.map
-import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -32,7 +29,6 @@ class AssignedJobsUseCase @Inject constructor(
   }
 
   private suspend fun getAssignedDeliveries(assignedId: String): Flow<Result<List<ActiveJobItems>>> {
-    Timber.e("assigned Id $assignedId")
     return assignedDataSource.getAssignedDeliveries(assignedId).map { result ->
       if (result is Result.Success) {
         val deliverItems = result.data.map { assignedJobsMapper.invoke(it) }
