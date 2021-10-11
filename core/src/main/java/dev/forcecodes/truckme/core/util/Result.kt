01a5b3c1e9c2294b43559ef6b98f3d34be5137ff
4s <fporquillo18@gmail.com>
2021-10-11 22:57:51 +0800

    Update Result.kt

--- core/src/main/java/dev/forcecodes/truckme/core/util/Result.kt
+++ core/src/main/java/dev/forcecodes/truckme/core/util/Result.kt
@@ -16,7 +16,7 @@ sealed class Result<out R> {
     return when (this) {
       is Success<*> -> "Success[data=$data]"
       is Error -> "Error[exception=$exception]"
-      Loading -> "Loading"
+      is Loading -> "Loading"
     }
   }
 }
