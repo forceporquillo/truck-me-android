5b9a01a57c85f1910f6f8fe13abb3d8633027877
strongforce1 <fporquillo18@gmail.com>
2021-10-29 23:05:22 +0800

    Add directions domain

---
+++ core/src/driver/java/dev/forcecodes/truckme/core/domain/directions/DirectionsRepository.kt
@@ -0,0 +1,41 @@
+package dev.forcecodes.truckme.core.domain.directions
+
+import dev.forcecodes.truckme.core.data.directions.DirectionsApiService
+import dev.forcecodes.truckme.core.data.directions.DirectionsResponse
+import dev.forcecodes.truckme.core.di.IoDispatcher
+import dev.forcecodes.truckme.core.model.LatLngTruckMeImpl
+import dev.forcecodes.truckme.core.util.ApiResponse
+import dev.forcecodes.truckme.core.util.BaseRepository
+import dev.forcecodes.truckme.core.util.DirectionsBackendApi
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+interface DirectionsRepository {
+  fun getDirections(
+    path: DirectionPath
+  ): Flow<ApiResponse<DirectionsResponse>>
+}
+
+class DirectionsRepositoryImpl @Inject constructor(
+  @DirectionsBackendApi private val directionsApiService: DirectionsApiService,
+  @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : BaseRepository(dispatcher), DirectionsRepository {
+
+  override fun getDirections(
+    path: DirectionPath
+  ) = flow {
+    val directionsResult =
+      directionsApiService.getDirections(
+        latLngTruckMeImplData = path.origin,
+        placeId = "place_id:${path.destinationPlaceId}"
+      )
+    emit(getResult { directionsResult })
+  }
+}
+
+data class DirectionPath(
+  val origin: LatLngTruckMeImpl,
+  val destinationPlaceId: String
+)
---
+++ core/src/driver/java/dev/forcecodes/truckme/core/domain/directions/GetDirectionsUseCase.kt
@@ -0,0 +1,32 @@
+package dev.forcecodes.truckme.core.domain.directions
+
+import dev.forcecodes.truckme.core.data.directions.DirectionsResponse
+import dev.forcecodes.truckme.core.di.IoDispatcher
+import dev.forcecodes.truckme.core.domain.FlowUseCase
+import dev.forcecodes.truckme.core.util.Result
+import dev.forcecodes.truckme.core.util.Result.Loading
+import dev.forcecodes.truckme.core.util.mapApiRequestResults
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetDirectionsUseCase @Inject constructor(
+  private val directionsRepository: DirectionsRepository,
+  @IoDispatcher private val dispatcher: CoroutineDispatcher
+) : FlowUseCase<DirectionPath, DirectionsResponse>(dispatcher) {
+
+  override fun execute(parameters: DirectionPath): Flow<Result<DirectionsResponse>> {
+    return flow {
+      emit(Loading)
+      val directionsApiResponse = directionsRepository
+        .getDirections(parameters)
+        .map { directionsApiResponse ->
+          directionsApiResponse.mapApiRequestResults { "Empty Api Response" }
+        }
+      emitAll(directionsApiResponse)
+    }
+  }
+}
