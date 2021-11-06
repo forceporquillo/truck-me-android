143b05e3d8b89002de4a9110f8a42620687484d8
strongforce1 <fporquillo18@gmail.com>
2021-11-07 06:41:41 +0800

    Synchronize profile upload

--- core/src/main/java/dev/forcecodes/truckme/core/domain/settings/UploadProfileUseCase.kt
+++ core/src/main/java/dev/forcecodes/truckme/core/domain/settings/UploadProfileUseCase.kt
@@ -1,22 +1,35 @@
 package dev.forcecodes.truckme.core.domain.settings
 
+import android.net.Uri
 import com.google.firebase.auth.ktx.userProfileChangeRequest
+import com.google.firebase.firestore.FirebaseFirestore
 import dev.forcecodes.truckme.core.data.auth.FirebaseAuthStateDataSource
 import dev.forcecodes.truckme.core.data.cloud.CloudStorageDataSource
+import dev.forcecodes.truckme.core.data.driver.RegisteredDriverDataSource
+import dev.forcecodes.truckme.core.di.ApplicationScope
 import dev.forcecodes.truckme.core.di.IoDispatcher
 import dev.forcecodes.truckme.core.domain.UseCase
+import dev.forcecodes.truckme.core.util.Result
 import dev.forcecodes.truckme.core.util.TaskData
 import dev.forcecodes.truckme.core.util.downloadUrl
+import dev.forcecodes.truckme.core.util.driverCollection
+import dev.forcecodes.truckme.core.util.isDriver
 import dev.forcecodes.truckme.core.util.triggerOneShotListener
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UploadProfileUseCase @Inject constructor(
+  private val firestore: FirebaseFirestore,
   private val authStateDataSource: FirebaseAuthStateDataSource,
   private val cloudStorageDataSource: CloudStorageDataSource,
+  private val registeredDriverDataSource: RegisteredDriverDataSource,
+  @ApplicationScope private val externalScope: CoroutineScope,
   @IoDispatcher val dispatcher: CoroutineDispatcher
 ) : UseCase<ProfileData, UploadResult>(dispatcher) {
 
@@ -41,12 +54,32 @@ class UploadProfileUseCase @Inject constructor(
       .downloadUrl(userId)
 
     downloadUriResult?.let { uri ->
+      if (isDriver) {
+        synchronizeDriverProfileUpdate(userId, uri)
+      }
+
       authStateDataSource.updateProfile(
         userProfileChangeRequest {
           photoUri = uri
         }
       )
     }
+  }
+
+  private fun synchronizeDriverProfileUpdate(authId: String, uri: Uri) {
+    externalScope.launch(dispatcher) {
+      registeredDriverDataSource.getUUIDbyAuthId(authId).collect { result ->
+        if (result is Result.Success) {
+          executeProfileUpdate(result.data, uri)
+        }
+      }
+    }
+  }
+
+  private fun executeProfileUpdate(driverDocumentId: String, uri: Uri) {
+    firestore.driverCollection()
+      .document(driverDocumentId)
+      .update(mapOf("profile" to uri.toString()))
   }
 }
 
