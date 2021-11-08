24d2541d8d2057850280867d8e1506c8a599f758
strongforce1 <fporquillo18@gmail.com>
2021-11-08 11:16:44 +0800

    Change `ItemDelivered` to `DeliveryInfo`

--- app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsFragmentListener.kt
+++ app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsFragmentListener.kt
@@ -1,7 +1,7 @@
 package dev.forcecodes.truckme.ui.statistics
 
-import dev.forcecodes.truckme.core.model.ItemDelivered
+import dev.forcecodes.truckme.core.model.DeliveryInfo
 
 interface StatisticsFragmentListener {
-  fun onChangeSearch(items: List<ItemDelivered>)
+  fun onChangeSearch(items: List<DeliveryInfo>)
 }
