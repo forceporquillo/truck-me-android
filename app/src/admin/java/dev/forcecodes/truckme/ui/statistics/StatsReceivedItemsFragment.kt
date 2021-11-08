2409da645267e1b015220a9ba3874c6d42225e78
strongforce1 <fporquillo18@gmail.com>
2021-11-08 11:16:11 +0800

    Fix mapping of items and filtering

--- app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatsReceivedItemsFragment.kt
+++ app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatsReceivedItemsFragment.kt
@@ -1,11 +1,12 @@
 package dev.forcecodes.truckme.ui.statistics
 
+import dev.forcecodes.truckme.core.model.DeliveryInfo
 import dev.forcecodes.truckme.core.model.ItemDelivered
 
 class StatsReceivedItemsFragment : StatsPagerFragment() {
 
-  override fun onChangeSearch(items: List<ItemDelivered>) {
-    adapter.setItems(items.filter { it.deliveryInfo?.inbound == true }
-      .map { it.deliveryInfo?.items ?: "" })
+  override fun onChangeSearch(items: List<DeliveryInfo>) {
+    adapter.setItems(items.filter { it.inbound == true }
+      .map { it.items })
   }
 }
