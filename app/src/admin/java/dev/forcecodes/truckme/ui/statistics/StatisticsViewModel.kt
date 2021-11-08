6a33237272c2f747d2c824773895ca2b469241ab
strongforce1 <fporquillo18@gmail.com>
2021-11-08 11:16:27 +0800

    Revamp `StatisticsViewModel`

--- app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsViewModel.kt
+++ app/src/admin/java/dev/forcecodes/truckme/ui/statistics/StatisticsViewModel.kt
@@ -4,17 +4,17 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dev.forcecodes.truckme.core.data.delivery.DeliveredItemDataSource
+import dev.forcecodes.truckme.core.data.delivery.convertToDate
 import dev.forcecodes.truckme.core.data.fleets.FleetUiModel.DriverUri
-import dev.forcecodes.truckme.core.domain.fleets.AdminDailySales
 import dev.forcecodes.truckme.core.domain.fleets.DailyStatisticsUseCase
 import dev.forcecodes.truckme.core.domain.fleets.ObserveDriverFleetsUseCase
-import dev.forcecodes.truckme.core.domain.fleets.formattedDate
-import dev.forcecodes.truckme.core.model.ItemDelivered
+import dev.forcecodes.truckme.core.model.DeliveryInfo
 import dev.forcecodes.truckme.core.util.Result
 import dev.forcecodes.truckme.ui.auth.signin.SignInViewModelDelegate
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -28,10 +28,6 @@ class StatisticsViewModel @Inject constructor(
 ) : ViewModel(), SignInViewModelDelegate by signInViewModelDelegate {
 
   var type: String = DAY
-    set(value) {
-      field = value
-      searchBy(userIdValue ?: return)
-    }
 
   private val _driverList = MutableStateFlow<List<String>>(emptyList())
   val driverList = _driverList.asStateFlow()
@@ -39,54 +35,63 @@ class StatisticsViewModel @Inject constructor(
   private val _dateList = MutableStateFlow<List<String>>(emptyList())
   val dateList = _dateList.asStateFlow()
 
-  private val _itemDelivered = MutableStateFlow<List<ItemDelivered>>(emptyList())
+  private val _itemDelivered = MutableStateFlow<List<String>>(emptyList())
   val itemDelivered = _itemDelivered.asStateFlow()
 
-  fun searchBy(field: String) {
+  private val _deliveryInfo = MutableStateFlow<List<DeliveryInfo>>(emptyList())
+
+  private val _copyDeliveryInfo = MutableStateFlow<List<DeliveryInfo>>(emptyList())
+  val copyDeliveryInfo = _copyDeliveryInfo.asStateFlow()
+
+  init {
     viewModelScope.launch {
-      if (type == DAY) {
-        _driverList.value = emptyList()
-        dailyStatisticsUseCase(AdminDailySales(userIdValue ?: return@launch, field)).collect {
-          Timber.e("Result $it")
-          if (it is Result.Success) {
-            _dateList.value = if (it.data.isEmpty()) {
-              listOf(formattedDate(System.currentTimeMillis()))
-            } else {
-              it.data
-            }
+      launch source@{
+        deliveredItemDataSource.getAllDeliveredItems(userIdValue ?: return@source).map { list ->
+          _dateList.value = list.map { deliveredItem ->
+            convertToDate(
+              "MM/dd/yyyy",
+              timeStampMillis = deliveredItem.timestamp
+            ) ?: ""
           }
-          if (it is Result.Error) {
-            _dateList.value = listOf(formattedDate(System.currentTimeMillis()))
-          }
+          list
+        }.collect {
+          _deliveryInfo.value = it
         }
       }
-      if (type == DRIVER) {
-        _dateList.value = emptyList()
-        observeDriverFleetsUseCase(userIdValue ?: return@launch).collect { result ->
-          _driverList.mapFleetList(result)
+      launch fleets@{
+        observeDriverFleetsUseCase(userIdValue ?: return@fleets).collect {
+          _driverList.mapFleetList(it)
         }
       }
     }
   }
 
-  fun executeQuery(newItem: String) {
-    _itemDelivered.value = emptyList()
-    viewModelScope.launch {
-      deliveredItemDataSource.getAllDeliveredItems(userIdValue ?: return@launch).collect {
-        if (type == DAY) {
-          _itemDelivered.value =
-            it.filter { info -> formattedDate(info.timestamp.toLong()) == newItem }
-        }
-        if (type == DRIVER) {
-          _itemDelivered.value =
-            it.filter { info -> info.deliveryInfo?.driverData?.driverName == newItem }
-        }
-      }
+  fun reOrder() {
+    if (type == DAY) {
+      _itemDelivered.value =
+        _deliveryInfo.value.map { convertToDate("MM/dd/yyyy", it.timestamp) ?: "" }.distinct()
     }
+
+    if (type == DRIVER) {
+      _itemDelivered.value = driverList.value
+    }
+
+    Timber.e("items ${_itemDelivered.value}")
   }
 
   private fun MutableStateFlow<List<String>>.mapFleetList(result: Result<List<DriverUri>>) {
     value = if (result is Result.Success) result.data.map { it.fullName } else emptyList()
+  }
+
+  fun executeQuery(newItem: String) {
+    if (type == DAY) {
+      _copyDeliveryInfo.value = _deliveryInfo.value.filter { convertToDate("MM/dd/yyyy", it.timestamp) == newItem }
+    }
+
+    if (type == DRIVER) {
+      _copyDeliveryInfo.value = _deliveryInfo.value.filter { it.driverData?.driverName == newItem }
+    }
+
   }
 
   companion object {
