d5043d56ff67307a61299940cc90ee4ed490060b
strongforce1 <fporquillo18@gmail.com>
2021-11-28 22:47:33 +0800

    Update TokenSharedPrefs.kt

--- core/src/main/java/dev/forcecodes/truckme/core/util/TokenSharedPrefs.kt
+++ core/src/main/java/dev/forcecodes/truckme/core/util/TokenSharedPrefs.kt
@@ -4,20 +4,17 @@ import android.content.Context
 import android.content.SharedPreferences
 
 fun Context.storeAdminToken(token: String) {
-  if (isDriver) {
-    throw IllegalStateException("Only admin build can request this method.")
-  }
   val prefs = sharedTokenPrefs()
   prefs.edit().putString("admin_token", token).apply()
 }
 
 fun Context.getAdminToken(): String? {
-  if (isDriver) {
-    throw IllegalStateException("Only admin build can request this method.")
-  }
   return sharedTokenPrefs().getString("admin_token", null)
 }
 
 private fun Context.sharedTokenPrefs(): SharedPreferences {
+  if (isDriver) {
+    throw IllegalStateException("Only admin build can request this method.")
+  }
   return getSharedPreferences("admin_token_prefs", Context.MODE_PRIVATE)
 }
